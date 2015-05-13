727997743bdc92894189dbeade15bc28cf4c286d
scbzyhx <scbzyhx@hotmail.com>
2015-05-13 21:05:05 +0800

    before rm test.md

--- content/awk-trans.md
+++ content/awk-trans.md
@@ -1,8 +1,8 @@
-Title: awk 教程
+Title: AWK 教程
 Date: 2015-05-13
 Modified:2015-05-13
 Category: 翻译
-Tags: 翻译，awk
+Tags: 翻译, awk
 Slug:awk-trans
 Author: 杨海祥
 
@@ -10,6 +10,6 @@ Author: 杨海祥
 
 该教程是为极客学院翻译的项目。
 
-翻译教程地址：https://github.com/scbzyhx/awk 或者 https://github.com/wenquan0hf/awk  
+翻译教程地址：<a href = "https://github.com/scbzyhx/awk">https://github.com/scbzyhx/awk</a> 或者  <a href = "https://github.com/wenquan0hf/awk ">https://github.com/wenquan0hf/awk </a>
 
-官方英文原版：http://www.tutorialspoint.com/awk/awk_overview.htm
+官方英文原版：<a href = "http://www.tutorialspoint.com/awk/awk_overview.htm">http://www.tutorialspoint.com/awk/awk_overview.htm </a>
--- pelicanconf.py
+++ pelicanconf.py
@@ -4,16 +4,16 @@ from __future__ import unicode_literals
 
 AUTHOR = u'scbzyhx'
 SITENAME = u'YANG'
-SITEURL = ''
+SITEURL = u'http://scbzyhx.github.io'
 
 PATH = 'content'
 
-TIMEZONE = 'Europe/Paris'
+TIMEZONE = 'Asia/Shanghai'
 
-DEFAULT_LANG = u'zhs'
+DEFAULT_LANG = u'zh'
 
-THEME = 'pelican-themes/elegant'
-DISQUS_SITENAME = 'yhx'
+THEME = 'pelican-themes/pelican-bootstrap3'
+DISQUS_SITENAME = u'yhx'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -23,16 +23,47 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (#('weibo', 'http://weibo.com/u/3445058592/'),
+         ('Github', 'http://github.com/'),
+         #('Jinja2', 'http://jinja.pocoo.org/'),
+         #('You can modify those links in your config file', '#'),
+         )
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('weibo', 'http://weibo.com/u/3445058592/'),
+         ('Github', 'http://github.com/scbzyhx'),)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+PLUGIN_PATHS = [u'pelican-plugins'] #plugin path
+PLUGINS = ['sitemap','random_article','neighbors'] # plugins enabled
+
+SITEMAP = {
+	"format":"xml",
+	"priorities":{
+		"articles":0.7,
+		"indexes":0.5,
+		"pages":0.3,
+	},
+	"changefreqs":{
+		"articles":"monthly",
+		"indexes":"daily",
+		"pages":"monthly",
+	}
+}
+#random to some blog
+RANDOM = 'random.html'
+
+RELATED_POSTS_MAX = 10
+
+#STATIC FILES
+#FILE_TO_COPY setting has been removed in favor of STATIC_PATHS and EXTRA_PATH_METADATA, 
+#see https://github.com/getpelican/pelican/blob/master/docs/settings.rst#path-metadata
+#FILES_TO_COPY = {
+	#("extra/robots.txt","robots.txt")
+#}
+#STATIC DIRS
+#STATIC_PATH = [u'img']
